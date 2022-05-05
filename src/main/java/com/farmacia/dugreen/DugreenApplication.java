2a59af5565e875799857e80da48d461c91f42491
Julio Barros <juliocezarbarros2@gmail.com>
2022-05-04 22:08:31 -0300

    CRUD completo, atividade Farmacia;

---
+++ src/main/java/com/farmacia/dugreen/DugreenApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.dugreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DugreenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/farmacia/dugreen/repository/CategoriaRepository.java
@@ -0,0 +1,17 @@
+package com.farmacia.dugreen.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.farmacia.dugreen.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findAllByCategoriaProdutoContainingIgnoreCase (@Param("categoriaProduto")String categoriaProduto);
+
+}
